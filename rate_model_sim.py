8c68e980c26b6c6325fdbbf9c87b27376314223b
Max Hasslberger <max@ceregate.com>
2023-02-20 10:51:11 +0100

    Higher order input - linear adjustment

--- rate_model_sim.py
+++ rate_model_sim.py
@@ -5,7 +5,7 @@ from itertools import combinations
 import matplotlib.pyplot as plt
 
 
-def run_sim(n_units, i_t, baseline, vip_in, f_flag, d_flag, dt, steps, v_flag):
+def run_sim(n_units, i_t, baseline, vip_in, q_thal, q_vip, f_flag, d_flag, dt, steps, v_flag):
     ############################################################
     # Init
     ############################################################
@@ -40,7 +40,7 @@ def run_sim(n_units, i_t, baseline, vip_in, f_flag, d_flag, dt, steps, v_flag):
     a_fac = np.array([[0.0, -0.0, -0.0, -0.0], [0, -0, -0, -0], [0.18, -0, -0, -0.05], [0.03, -0, -0.28, -0.04]])
 
     # thalamic input
-    q = 5
+    # q = 5
     alpha = 0.65
     tau_d1 = 1500 * 1e-3  # s
     tau_d2 = 20 * 1e-3  # s
@@ -103,7 +103,7 @@ def run_sim(n_units, i_t, baseline, vip_in, f_flag, d_flag, dt, steps, v_flag):
         f_rates_tmp = np.tile(f_rates[i, :, :], (n_subtypes, 1, 1))
 
         f_arg = np.sum(tmp * f_rates_tmp, axis=1) + cross_exc_in \
-                + thal_flag * q * (thal_arg[i + 1, :] + cross_thal) + vip_flag * vip_in[i, :n_units]
+                + thal_flag * q_thal * (thal_arg[i + 1, :] + cross_thal) + vip_flag * q_vip * vip_in[i, :n_units]
         d_dt = (-f_rates[i, :, :] + f_function(f_arg - thresholds)) / tau
         f_rates[i + 1, :, :] = forward_euler(d_dt, f_rates[i, :, :], dt)  # update firing rates
         # f_rates[i + 1, 3, :] = vip_in[i, :n_units]
@@ -146,8 +146,8 @@ def exe_wilson_cowan():
     # inter_stim_dur = 600 * 1e-3
     # inter_trial_dur = 1400 * 1e-3
     trial_pulses = 20
-    magnitude = 0.1
-    i_t = magnitude * cont_pulse_trials(0, stim_dur, inter_stim_dur, inter_trial_dur, trial_pulses, steps, dt)
+    q_thal = 0.1
+    i_t = cont_pulse_trials(0, stim_dur, inter_stim_dur, inter_trial_dur, trial_pulses, steps, dt)
     # i_t = i_t - 0.5 * magnitude * cont_pulse_trials(0, stim_dur, inter_stim_dur, 8300 * 1e-3, 1, steps, dt)
     # i_t[84000:85000] = 0.5 * magnitude  # higher order impacting input plasticity (depressive)
     i_t = np.tile(i_t, (no_of_units, 1))
@@ -158,10 +158,10 @@ def exe_wilson_cowan():
     stim_dur = 20 * 1e-3
     inter_stim_dur = 380 * 1e-3
     inter_trial_dur = 780 * 1e-3
-    magnitude = 0.3
-    vip_in = magnitude * cont_pulse_trials(0, stim_dur, inter_stim_dur, inter_trial_dur, trial_pulses, steps, dt)
+    q_vip = 0.5
+    vip_in = cont_pulse_trials(0, stim_dur, inter_stim_dur, inter_trial_dur, trial_pulses, steps, dt)
     # vip_in[int(steps / 2):] = vip_in[int(steps / 2):] / 1.5
-    # vip_in = vip_in + 1 * cont_pulse_trials(1, 400 * 1e-3, inter_stim_dur, 7600 * 1e-3, 1, steps, dt)
+    vip_in = vip_in + 2* cont_pulse_trials(1, 400 * 1e-3, inter_stim_dur, 7600 * 1e-3, 1, steps, dt) # probier länger anhaltende ramp - sst bei stim down
     # vip_in[84000:84300] = 1
     # vip_in[84000:84200] = 1.5
     # vip_in = vip_in + 0.5 * cont_pulse_trials(0, 350 * 1e-3, inter_stim_dur, 2900 * 1e-3 + 100 * 1e-3, 1, steps, dt)
@@ -169,7 +169,8 @@ def exe_wilson_cowan():
     vip_in = np.tile(vip_in, (no_of_units, 1))
     vip_in = np.swapaxes(vip_in, 0, 1)
 
-    [f_rates, thal_input, F, D] = run_sim(no_of_units, i_t, baseline, vip_in, f_flag, d_flag, dt, steps, v_flag)
+    [f_rates, thal_input, F, D] = run_sim(no_of_units, i_t, baseline, vip_in, q_thal, q_vip,
+                                          f_flag, d_flag, dt, steps, v_flag)
 
     # d_flag = np.ones((4, 4))
     # d_flag = np.zeros((4, 4))
@@ -218,8 +219,8 @@ def exe_wilson_cowan():
 
     time = np.arange(0, t_ges, dt)
 
-    plt.plot(time, i_t[:, 0])
-    plt.plot(time, vip_in[:, 0])
+    plt.plot(time, q_thal * i_t[:, 0])
+    plt.plot(time, q_vip * vip_in[:, 0])
 
     plt.legend(['Stimulus', 'Higher Order'])
     plt.title("Input signals")
